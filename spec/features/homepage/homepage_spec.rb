88df153362ec9713a6781028eb0ee76e09f8b49f
Maciej N <magnax@gmail.com>
2017-12-22 23:36:16 +0100

    Changes in home index view

--- spec/features/homepage/homepage_spec.rb
+++ spec/features/homepage/homepage_spec.rb
@@ -1,8 +1,37 @@
 require 'rails_helper'
 
 describe 'home page' do
-  it 'shows title' do
-    visit '/'
-    expect(page).to have_content 'N.P. ;-)'
+  context 'elements' do
+    it 'has title' do
+      visit '/'
+      expect(page).to have_content 'N.P. ;-)'
+    end
+
+    it 'has a form to send information' do
+      visit '/'
+      expect(page).to have_selector('input[name="text"]')
+      expect(page).to have_css('#info-text-box')
+      expect(page).to have_selector('button[type="submit"]')
+    end
+  end
+
+  context 'functionality - sending info' do
+    context 'valid info' do
+      it 'submit information from input' do
+        visit '/'
+        fill_in 'info-text-box', with: 'Some info'
+        click_button 'send-button'
+        expect(page).to have_content 'Thanks!'
+      end
+    end
+
+    context 'invalid info' do
+      it 'submit information from input' do
+        visit '/'
+        fill_in 'info-text-box', with: ''
+        click_button 'send-button'
+        expect(page).to have_css '.site-messages-error', text: 'Error'
+      end
+    end
   end
 end
