ca0fbc8866d792114972a53448c17ac153c27a95
Maciej N <magnax@gmail.com>
2017-12-27 19:23:31 +0100

    Very basic admin home controller

---
+++ app/controllers/admin/home_controller.rb
@@ -0,0 +1,4 @@
+module Admin
+  class HomeController < ActionController::Base
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   resources :informations, only: [:create]
+
+  namespace :admin do
+    root to: 'home#index'
+  end
   
   root to: 'home#index'
 end
