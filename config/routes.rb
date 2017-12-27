049003ba89b8a98b98bda9cc1a59b68d6f33b41d
Maciej N <magnax@gmail.com>
2017-12-27 22:20:42 +0100

    Route for informations

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :informations, only: [:create]
 
   namespace :admin do
+    resources :informations, only: [:index, :show]
     root to: 'home#index'
   end
   
