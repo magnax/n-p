2b1c1a5932ffe6fab9d3aa53d1bf1d882356ddc7
Maciej N <magnax@gmail.com>
2018-12-08 14:05:06 +0100

    Build Person model as neo4j node using rails scaffolding

---
+++ app/models/person.rb
@@ -0,0 +1,10 @@
+class Person 
+  include Neo4j::ActiveNode
+  property :first_name, type: String
+  property :last_name, type: String
+  property :pesel, type: String
+  property :date_of_birth, type: Date
+
+
+
+end
---
+++ spec/views/people/index.html.haml_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe "people/index", type: :view do
+  before(:each) do
+    assign(:people, [
+      Person.create!(
+        :first_name => "First Name",
+        :last_name => "Last Name",
+        :pesel => "Pesel"
+      ),
+      Person.create!(
+        :first_name => "First Name",
+        :last_name => "Last Name",
+        :pesel => "Pesel"
+      )
+    ])
+  end
+
+  it "renders a list of people" do
+    render
+    assert_select "tr>td", :text => "First Name".to_s, :count => 2
+    assert_select "tr>td", :text => "Last Name".to_s, :count => 2
+    assert_select "tr>td", :text => "Pesel".to_s, :count => 2
+  end
+end
