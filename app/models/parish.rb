f9e7d353241655ef228acd1deb4669c5989fc5c7
Maciej N <magnax@gmail.com>
2018-12-11 22:04:29 +0100

    Add list to parish model

--- app/models/parish.rb
+++ app/models/parish.rb
@@ -5,5 +5,7 @@ class Parish
   has_one :out, :town, type: :LOCATED
   has_one :out, :diocese, type: :BELONGS_TO
 
-
+  def self.list_all
+    Parish.all.map { |p| ["#{p.town.name} | #{p.name}", p.id] }
+  end
 end
